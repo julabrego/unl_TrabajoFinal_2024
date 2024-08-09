ae20a2fa810b584e4d0987c8a31af25799d7627c
Julián Abrego <abrego.jul@gmail.com>
2024-08-08 21:42:48 -0300

    feat: grunt enemy KetchupSmall

--- Assets/Scripts/Characters/KetchupSmall/ketchup_small.gd
+++ Assets/Scripts/Characters/KetchupSmall/ketchup_small.gd
@@ -1,13 +1,10 @@
-class_name EmpanadaPlayer
+class_name KetchupSmall
 extends CharacterBody3D
 
-@export var SPEED := 4
-@export var JUMP_FORCE := 8
+@export var SPEED := 6
+@export var JUMP_FORCE := 5
 @export var GRAVITY := 19.8
-@export var attack := ["Attack_1"]
 
-var attack_index := -1
-var timer := 0.0
 var attacking := false
 
 var motion := Vector3()
@@ -22,14 +19,11 @@ func _process(delta) -> void:
 	if not is_on_ground():
 		motion.y -= GRAVITY * delta
 	
-	motion.x = Input.get_axis("ui_left", "ui_right") * speed
-	motion.z = Input.get_axis("ui_up", "ui_down") * speed
+	#motion.x = Input.get_axis("ui_left", "ui_right") * speed
+	#motion.z = Input.get_axis("ui_up", "ui_down") * speed
 	
-	if Input.is_action_just_pressed("ui_jump") and is_on_ground():
+	if is_on_ground():
 		motion.y = JUMP_FORCE
-		attacking = false
-	elif Input.is_action_just_pressed("ui_attack") and is_on_ground() and not attacking:
-		punch()
 	
 	_flip()
 	_animations(delta)
@@ -43,16 +37,8 @@ func _flip() -> void:
 		sprite.flip_h = false if motion.x > 0 else true
 
 func _animations(delta : float) -> void:
-	if is_on_ground():
-		if (attacking):
-			stop_movement()
-		elif motion.x != 0 || motion.z != 0:
-			set_animation("idle")
-		else:
-			set_animation("idle")
-	#else:
-		#_set_animation("Jump")
-
+	pass
+	
 func stop_movement() -> void:
 	motion.x = 0
 	motion.z = 0
@@ -64,11 +50,3 @@ func set_animation(anim: String) -> void:
 	if animation != anim:
 		animation = anim
 		animation_player.play(animation)
-
-func punch() -> void:
-	attacking = true
-	attack_index = (attack_index + 1) % attack.size()
-	set_animation(attack[attack_index])
-
-func end_attack() -> void:
-	attacking = false
